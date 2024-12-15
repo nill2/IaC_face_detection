6abfc9c95c9352905ea65707b90809523de28de1
Danil Kabanov <nill2@yandex.ru>
2024-12-15 16:33:28 +0100

    feat:  sh script
    what: updated sh script to setup docker and launch face detection
    use apt

--- cloud-init-script.sh
+++ cloud-init-script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y docker.io
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker $USER
+docker --version
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker pull nill2/face_detection:latest
-sudo docker run -d -p 80:5000 nill2/face_detection:latest
+sudo docker run -d nill2/face_detection:latest
